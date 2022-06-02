d870cf77816b432f8938750457549aa256a2390d
CFeeney333 <c.feeney333@gmail.com>
2022-06-02 12:52:32 +0100

    Create buttons for html

--- calculator.js
+++ calculator.js
@@ -1,3 +1,27 @@
+const body = document.querySelector("body");
+
+const expression = [];
+
+for (let i = 0; i < 10; i++) {
+  const element = document.createElement("div");
+  element.textContent = i;
+  element.id = i;
+  element.addEventListener("click", onKeyPress);
+  body.appendChild(element);
+}
+
+for (let op of ["+", "-", "*", "/", "="]) {
+  const element = document.createElement("div");
+  element.textContent = op;
+  element.id = op;
+  element.addEventListener("click", onKeyPress);
+  body.appendChild(element);
+}
+
+function onKeyPress(e) {
+  expression.push(e.target.id);
+}
+
 function add(a, b) {
   return a + b;
 }
